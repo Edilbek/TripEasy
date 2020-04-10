5301e0f3da40ce5ba8215a6d5f061514c2602bcb
Edilbek <edil.talantbekov@gmail.com>
2020-04-10 15:37:28 +0600

    bcrypt user password

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import {getRepository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
+import * as bcrypt from "bcryptjs";
 import {User} from "../entity/User";
 
 export class UserController {
@@ -15,13 +16,15 @@ export class UserController {
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
+        let user = request.body;
+        user.password = bcrypt.hashSync(user.password, 8);
         return this.userRepository.save(request.body);
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
         let userToRemove = await this.userRepository.findOne(request.params.id);
         await this.userRepository.remove(userToRemove);
-        return "User deleted successfully";
+        return "User created successfully";
     }
 
 }
